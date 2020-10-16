e425eb8e9e3213f57280d60fd73b821012324fdf
Matthew Witman <mnwranger@gmail.com>
2020-10-15 20:23:23 -0400

    Added better error handling to the warp handlers

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,41 +1,50 @@
 use crate::models::Db;
 use serde_json::json;
 use serenity::http::Http;
-use std::convert::Infallible;
 
 pub async fn move_users_to_original(
     guild_id: String,
     token: String,
     db: Db,
-) -> Result<impl warp::Reply, Infallible> {
+) -> Result<impl warp::Reply, warp::Rejection> {
     let http = Http::new_with_token(&token);
     {
         let map = db.read().await;
 
-        let guild_vec = map.get(&guild_id).expect("Just for now");
+        let guild_vec = match map.get(&guild_id) {
+            Some(guild_vec) => guild_vec,
+            None => return Err(warp::reject::not_found()),
+        };
 
         for user in guild_vec.iter() {
             let mut map = serde_json::Map::new();
             map.insert("channel_id".to_string(), json!(user.original_channel));
 
-            http.edit_member(guild_id.parse().expect("We need this"), user.id, &map)
-                .await
-                .expect("That shouldn't have happened")
+            http.edit_member(
+                guild_id.parse().expect("This should be a number"),
+                user.id,
+                &map,
+            )
+            .await
+            .expect("That shouldn't have happened")
         }
     }
-    Ok(warp::reply::json(&"Moved two users"))
+    Ok(warp::reply::json(&"Moved users"))
 }
 
 pub async fn move_users_to_group(
     guild_id: String,
     token: String,
     db: Db,
-) -> Result<impl warp::Reply, Infallible> {
+) -> Result<impl warp::Reply, warp::Rejection> {
     let http = Http::new_with_token(&token);
     {
         let map = db.read().await;
 
-        let guild_vec = map.get(&guild_id).expect("Just for now");
+        let guild_vec = match map.get(&guild_id) {
+            Some(guild_vec) => guild_vec,
+            None => return Err(warp::reject::not_found()),
+        };
 
         println!("{:#?}", guild_vec);
 
@@ -44,7 +53,7 @@ pub async fn move_users_to_group(
             map.insert("channel_id".to_string(), json!(user.new_channel));
 
             http.edit_member(
-                guild_id.parse::<u64>().expect("Crashy crashy"),
+                guild_id.parse().expect("This should be a number"),
                 user.id,
                 &map,
             )
